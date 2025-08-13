3285d95e752cb545a5a31954d9ea96d7d030e2d4
YiYing Chiang (Elena) <elena.yychiang@gmail.com>
2025-08-13 16:21:14 +0100

    FINAL WORKING VERSION: python3 + static files + runtime.txt

--- backend/settings.py
+++ backend/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
